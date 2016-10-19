020691a1cd0cc0e7d7156a1cc48ea8bbee24ecf4
mariejinpark <mariejinpark@gmail.com>
2016-10-18 23:23:47 -0400

    ScrollViewLab

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -12,15 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+//        scrollView.delegate = self
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        updateMinZoomScale(for: view.bounds.size)
+//    }
 
 
 }
